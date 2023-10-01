58640edc7b338a554b872d1f3f212fba31b92931
Samuel Martins <samuel.ubuntu@gmail.com>
2023-10-01 16:42:30 -0300

    Initial Commit

---
+++ Reloaded II UI/Reloaded_II_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reloaded_II_UIApp.swift
+//  Reloaded II UI
+//
+//  Created by Samuel Martins on 01/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Reloaded_II_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
